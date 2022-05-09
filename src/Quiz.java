e6e08b3c979e3ee981a194f02ce3342feed29109
cam <cameron.prayle@gmail.com>
2022-05-09 15:24:14 +0100

    Quiz

--- src/Quiz.java
+++ src/Quiz.java
@@ -20,7 +20,7 @@ public class Quiz {
         }
         return(userIn);
     }
-    public static void login(){
+    public static String login(){
         boolean matchFound=false;
         boolean lockout=false;
         int attempts = 0;
@@ -37,7 +37,8 @@ public class Quiz {
             for(int i=0; i< usernames.length; i++){
                 if(inName.equals(usernames[i]) && inPass.equals(passwords[i])) {
                     matchFound = true;
-                    System.out.println("Welcome "+inName+"!");
+                    System.out.println("\nWelcome "+inName+"!");
+                    return(inName);
                 }
             }
 
@@ -49,6 +50,7 @@ public class Quiz {
                 }
             }
         }
+        return null;
     }
 
 
@@ -96,7 +98,8 @@ public class Quiz {
             Scanner userChoice = new Scanner(System.in);
             choice = userChoice.nextLine();
             if (choice.equals("1")) {
-                login();
+                String loggedUname = login();
+                quiz(loggedUname);
             } else if (choice.equals("2")) {
                 register();
             } else {
@@ -104,8 +107,45 @@ public class Quiz {
             }
         }
     }
+
+    public static void quiz(String uname){
+        int score=0;
+        System.out.println("\nToday's Quiz is going to be about Minecraft\n");
+        String question1[] = {"15","12","10","9"};
+        System.out.println("Question 1.\nHow many blocks does it take to make a nether portal?");
+        for (int i=0; i< question1.length; i++){
+            System.out.println((i+1)+". "+ question1[i]);
+        }
+        String answer = getNotBlank();
+        if(answer.equals("3")){
+            score++;
+        }
+        String question2[] = {"6","5","4","3"};
+        System.out.println("Question 2.\nHow many wooden planks does it take to make a boat?");
+        for (int i=0; i< question2.length; i++){
+            System.out.println((i+1)+". "+ question2[i]);
+        }
+        answer = getNotBlank();
+        if(answer.equals("2")){
+            score++;
+        }
+
+        String question3[] = {"Dont dig straight down","Always shoot creepers with a bow","Dont mine at night","Build house"};
+        System.out.println("Question 3.\nWhats rule 1 of minecraft?");
+        for (int i=0; i< question3.length; i++){
+            System.out.println((i+1)+". "+ question3[i]);
+        }
+        answer = getNotBlank();
+        if(answer.equals("1")){
+            score++;
+        }
+
+        System.out.println(uname+ " got a score of " + score);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Quiz");
+        System.out.println("\nWelcome to the Quiz");
         System.out.println("First you need an account");
+        accountChoice();
     }
 }
